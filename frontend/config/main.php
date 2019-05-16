cf6249f8e2d194d98b4f332af22bbd391b4df1a8
Cortess <CortessHack@gmail.com>
2019-05-16 19:29:08 +0300

    User module

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -11,6 +11,11 @@ return [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
+    'modules' => [
+        'user' => [
+            'class' => 'frontend\modules\user\Module',
+        ],
+    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
---
+++ frontend/modules/user/views/profile/view.php
@@ -0,0 +1,5 @@
+<?php
+
+/* @var $user frontend\models\User */
+
+echo "Hello ".$user->username;
